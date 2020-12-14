4438b2997335446e26fd8ec6c4484a29afb9912d
Clément DOUIN <soywod@users.noreply.github.com>
2020-12-14 20:22:19 +0100

    animate watch loader when disconnected

--- lib/utils.ml
+++ lib/utils.ml
@@ -1,4 +1,6 @@
-let lift_str str =
-  let last_char = str.[String.length str - 1]
-  and sub_str = String.sub str 0 (String.length str - 1) in
-  Printf.sprintf "%c%s" last_char sub_str
+let uppercase_at idx str =
+  let with_uppercase = function
+    | i when i == idx -> Char.uppercase_ascii
+    | _ -> Char.lowercase_ascii
+  in
+  String.mapi with_uppercase str
