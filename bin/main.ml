1e50d62cd70e60353077ca5aa263e8743b937f11
Kevin Schoon <kevinschoon@gmail.com>
2021-04-25 10:29:42 -0400

    add core.Command library (#10)

--- bin/main.ml
+++ bin/main.ml
@@ -1,29 +1,39 @@
 open Lib
+open Core
 
 let start () =
-  let pid = Process.start () in
-  Printf.printf "Timer started (pid=%d).\n" pid
+  Command.basic ~summary:"start a new pomodoro"
+    (Command.Param.return (fun () ->
+         let pid = Process.start () in
+         Printf.printf "Timer started (pid=%d).\n" pid))
 
 let run () =
-  let config = Config.read_file () in
-  let initial_timer = Timer.initial_timer config in
-  let broadcaster = Socket.create_and_accept () in
-  Timer.run config initial_timer broadcaster
+  Command.basic ~summary:"start and watch a pomodoro"
+    (Command.Param.return (fun () ->
+         let config = Config.read_file () in
+         let initial_timer = Timer.initial_timer config in
+         let broadcaster = Socket.create_and_accept () in
+         Timer.run config initial_timer broadcaster))
 
 let watch () =
-  let handler data = print_endline data in
-  Socket.connect_and_listen handler
+  Command.basic ~summary:"watch an existing pomodoro"
+    (Command.Param.return (fun () ->
+         let handler data = print_endline data in
+         Socket.connect_and_listen handler))
 
 let stop () =
-  Process.stop ();
-  print_endline "Timer stopped."
+  Command.basic ~summary:"stop a pomodoro"
+    (Command.Param.return (fun () ->
+         Process.stop ();
+         print_endline "Timer stopped."))
 
 let () =
-  let argv_last_idx = Array.length Sys.argv - 1 in
-  let arg_list = Array.to_list @@ Array.sub Sys.argv 1 argv_last_idx in
-  match arg_list with
-  | "start" :: _ -> start ()
-  | "run" :: _ -> run ()
-  | "watch" :: _ -> watch ()
-  | "stop" :: _ -> stop ()
-  | _ -> print_endline "Command not found"
+  Command.run
+    (Command.group
+       ~summary:"Socket-based CLI timer following the Pomodoro principles"
+       [
+         ("start", start ());
+         ("run", run ());
+         ("watch", watch ());
+         ("stop", stop ());
+       ])
