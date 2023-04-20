2424c45ad47014cb09fd221f4369b07514b24135
Clément DOUIN <clement.douin@posteo.net>
2023-04-21 00:23:25 +0200

    improve cross compilation, set up cachix

--- flake.nix
+++ flake.nix
@@ -1,56 +1,129 @@
 {
-  description = "CLI for managing your time using the Pomodoro technique, based on the pimalaya library.";
+  description = "CLI to manage your time using the Pomodoro technique.";
 
   inputs = {
-    utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    naersk.url = "github:nix-community/naersk";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, naersk, ... }:
-    utils.lib.eachDefaultSystem
-      (system:
+  outputs = { self, nixpkgs, flake-utils, gitignore, fenix, naersk }:
+    let
+      inherit (gitignore.lib) gitignoreSource;
+
+      mkToolchain = buildPlatform:
+        fenix.packages.${buildPlatform}.minimal.toolchain;
+
+      mkToolchainWithTarget = buildPlatform: targetPlatform:
+        with fenix.packages.${buildPlatform}; combine [
+          minimal.rustc
+          minimal.cargo
+          targets.${targetPlatform}.latest.rust-std
+        ];
+
+      mkDevShells = buildPlatform:
         let
-          name = "comodoro";
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
+          pkgs = import nixpkgs { system = buildPlatform; };
+          toolchain = fenix.packages.${buildPlatform}.default.withComponents [
+            "cargo"
+            "clippy"
+            "rustc"
+            "rustfmt"
+          ];
         in
-        rec {
-          # nix build
-          defaultPackage = packages.${name};
-          packages = {
-            ${name} = naersk.lib.${system}.buildPackage {
-              pname = name;
-              root = ./.;
-              nativeBuildInputs = with pkgs; [ openssl.dev pkg-config ];
-            };
-          };
-
-          # nix run
-          defaultApp = apps.${name};
-          apps.${name} = utils.lib.mkApp {
-            inherit name;
-            drv = packages.${name};
-          };
-
-          # nix develop
-          devShell = pkgs.mkShell {
-            inputsFrom = builtins.attrValues self.packages.${system};
-            nativeBuildInputs = with pkgs; [
-              # Nix LSP + formatter
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # Nix env
               rnix-lsp
               nixpkgs-fmt
 
               # Rust env
-              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-              cargo-watch
+              toolchain
               rust-analyzer
             ];
           };
-        }
-      );
+        };
+
+      mkPackage = pkgs: buildPlatform: targetPlatform: package:
+        let
+          toolchain =
+            if isNull targetPlatform
+            then mkToolchain buildPlatform
+            else mkToolchainWithTarget buildPlatform targetPlatform;
+          naersk' = naersk.lib.${buildPlatform}.override {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+          package' = {
+            name = "comodoro";
+            src = gitignoreSource ./.;
+          } // pkgs.lib.optionalAttrs (!isNull targetPlatform) {
+            CARGO_BUILD_TARGET = targetPlatform;
+          } // package;
+        in
+        naersk'.buildPackage package';
+
+      mkPackages = buildPlatform:
+        let
+          pkgs = import nixpkgs { system = buildPlatform; };
+          defaultPackage = mkPackage pkgs buildPlatform null { };
+          mkPackageWithTarget = mkPackage pkgs buildPlatform;
+        in
+        {
+          default = defaultPackage;
+          linux = defaultPackage;
+          macos = defaultPackage;
+          musl = mkPackageWithTarget "x86_64-unknown-linux-musl" {
+            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          };
+          windows = mkPackageWithTarget "x86_64-pc-windows-gnu" {
+            strictDeps = true;
+            depsBuildBuild = with pkgs.pkgsCross.mingwW64; [
+              stdenv.cc
+              windows.pthreads
+            ];
+          };
+        };
+
+      mkApp = drv: flake-utils.lib.mkApp {
+        inherit drv;
+        name = "comodoro";
+      };
+
+      mkApps = buildPlatform: {
+        default = mkApp self.packages.${buildPlatform}.default;
+        linux = mkApp self.packages.${buildPlatform}.linux;
+        macos = mkApp self.packages.${buildPlatform}.macos;
+        musl = mkApp self.packages.${buildPlatform}.musl;
+        windows =
+          let
+            pkgs = import nixpkgs { system = buildPlatform; };
+            wine = pkgs.wine.override { wineBuild = "wine64"; };
+            comodoro = self.packages.${buildPlatform}.windows;
+            app = pkgs.writeShellScriptBin "comodoro" ''
+              export WINEPREFIX="$(mktemp -d)"
+              ${wine}/bin/wine64 ${comodoro}/bin/comodoro.exe $@
+            '';
+          in
+          mkApp app;
+      };
+
+    in
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells = mkDevShells system;
+      packages = mkPackages system;
+      apps = mkApps system;
+    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,5 @@ pub mod man;
 mod protocol;
 pub mod server;
 
-pub use client::*;
 pub use config::*;
 pub use protocol::*;
